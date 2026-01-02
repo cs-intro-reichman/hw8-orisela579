aa2b335872568845532636f503ef42ddb75e2f70
Ori Sela <orisela579@gmail.com>
2026-01-02 18:59:47 +0200

    fourth :(

--- Network.java
+++ Network.java
@@ -99,9 +99,9 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String ans = "Network:\n";
+        String ans = "Network:";
         for(int i = 0; i < userCount; i++){
-         ans = ans + users[i].toString() + "\n";
+         ans = ans + "\n" +users[i].toString();
        }
        return ans;
     }
